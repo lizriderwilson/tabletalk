0321bffdc2be9c6a35dfcd369aeee559f9954847
Liz Wilson <lizriderwilson@gmail.com>
2021-05-23 13:31:55 -0400

    Setup config/routes with routes for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get 'static/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :campaigns, only: [:index]
+    resources :characters, only: [:index, :show, :edit, :update]
+  end
+
+  resources :campaigns do
+    resources :sessions, :characters
+  end
 end
