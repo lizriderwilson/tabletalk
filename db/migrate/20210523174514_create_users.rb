81ed36b374a729f8dedab90f7090d0514530e10f
Liz Wilson <lizriderwilson@gmail.com>
2021-05-24 00:09:04 -0400

    Add basic users#create method and add user view files to filetree

--- db/migrate/20210523174514_create_users.rb
+++ db/migrate/20210523174514_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email_address
+      t.string :email
       t.string :password_digest
       t.string :username
       t.text :bio
