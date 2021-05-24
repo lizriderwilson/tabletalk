e878ba89dff4d3752727ff51a68a69d0662a4918
Liz Wilson <lizriderwilson@gmail.com>
2021-05-24 00:09:04 -0400

    Add basic users#create method and add user view files to filetree

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to new_user_path unless @user.save
+        session[:user_id] = @user.id
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :username, :bio, :favorite_systems)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_05_23_174514) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address"
+    t.string "email"
     t.string "password_digest"
     t.string "username"
     t.text "bio"
