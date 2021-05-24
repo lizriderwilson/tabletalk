d0dcaca905f065055b0a86343cd199d2513d45ef
Liz Wilson <lizriderwilson@gmail.com>
2021-05-23 23:44:23 -0400

    Stub out SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'static/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Homepage
+  root 'static#index'
+
+  # Sessions
+  get '/login', to: 'sessions#new' 
+  post '/login', to: 'sessions#create'
 
   resources :users do
     resources :campaigns, only: [:index]
---
+++ db/migrate/20210523174514_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email_address
+      t.string :password_digest
+      t.string :username
+      t.text :bio
+      t.string :favorite_systems
+
+      t.timestamps
+    end
+  end
+end
