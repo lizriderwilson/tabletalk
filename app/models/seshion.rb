1e24fbccec3a2d13debb33ca579983a31ec1b326
Liz Wilson <lizriderwilson@gmail.com>
2021-05-30 13:06:33 -0400

    Create .oldest_to_newest and .newest_to_oldest methods for Seshion model

--- app/models/seshion.rb
+++ app/models/seshion.rb
@@ -1,3 +1,11 @@
 class Seshion < ApplicationRecord
     belongs_to :campaign
+
+    def self.oldest_to_newest
+        self.order(:start_time)
+    end
+
+    def self.newest_to_oldest
+        self.order(start_time: :desc)
+    end
 end
