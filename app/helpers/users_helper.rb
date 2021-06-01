6c48ea926aef8dea2f4d455ac5b8eb92bac9b361
Liz Wilson <lizriderwilson@gmail.com>
2021-06-01 12:37:24 -0400

    Add display_user_name helper to change display name on user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+    def display_user_name
+        if @user == current_user
+            "Your"
+        else
+            @user.username.capitalize + "'s"
+        end
+    end
+
 end
