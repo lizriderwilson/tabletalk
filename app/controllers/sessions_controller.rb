f959e23c37db19d0192f0304578f38639372ac18
Liz Wilson <lizriderwilson@gmail.com>
2021-06-05 12:34:39 -0400

    Add flash messages to login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,32 +4,39 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:user][:email])
-        return head(:forbidden) unless @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            log_in(user)
+        else
+            flash[:alert] = "Couldn't log in with that email/password combination."
+            render :new
+        end
     end
 
     def create_by_omniauth
         user = User.find_or_create_by_oauth(auth)
         if user.valid?
-            session[:user_id] = user.id
-            redirect_to user_path(user)
+            log_in(user)
         else
             flash[:message] = user.errors.full_messages.join(", ")
-            redirect_to new_user_path
+            render :new
         end
     end
 
     def destroy
         session.delete :user_id
-        redirect_to :root
+        redirect_to :root, notice: "Successfully logged out."
     end
 
     private
 
     def auth
         request.env['omniauth.auth']
+    end
+
+    def log_in(user)
+        session[:user_id] = user.id
+        redirect_to user_path(user), notice: "Successfully logged in!"
     end
 
 end
